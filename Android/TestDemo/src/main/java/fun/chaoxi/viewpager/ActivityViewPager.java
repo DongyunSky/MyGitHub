243b997a8c538d27afaec3c1b85bce304a62558c
wing <DongyunSky@163.com>
2018-11-06 18:29:43 +0800

    Test ViewPager

---
+++ Android/TestDemo/src/main/java/fun/chaoxi/viewpager/ActivityViewPager.java
@@ -0,0 +1,30 @@
+package fun.chaoxi.viewpager;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fun.chaoxi.testdemo.R;
+
+/**
+ * Create by Wing Gao on 2018/11/6 13:14
+ */
+public class ActivityViewPager extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.view_pager_view_pager);
+
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            list.add("第" + i + "个View");
+        }
+        ViewPager vp = (ViewPager) findViewById(R.id.view_pager);
+        vp.setAdapter(new AdapterViewPager(this, list));
+    }
+}
---
+++ Android/TestDemo/src/main/java/fun/chaoxi/viewpager/AdapterViewPager.java
@@ -0,0 +1,54 @@
+package fun.chaoxi.viewpager;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import fun.chaoxi.testdemo.R;
+
+/**
+ * Create by Wing Gao on 2018/11/6 13:23
+ */
+public class AdapterViewPager extends PagerAdapter {
+
+    private Context context;
+    private List<String> stringList;
+
+    public AdapterViewPager(Context context, List<String> stringList) {
+        this.context = context;
+        this.stringList = stringList;
+    }
+
+    /**
+     * @return viewPage的个数
+     */
+    @Override
+    public int getCount() {
+        return stringList.size();
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View) object);
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        View view = View.inflate(context, R.layout.pager_view_page, null);
+        TextView textView = view.findViewById(R.id.view_pager_text_view);
+        textView.setText(stringList.get(position)); // 通过当前viewPager下标
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
+        return view == o;
+    }
+}
