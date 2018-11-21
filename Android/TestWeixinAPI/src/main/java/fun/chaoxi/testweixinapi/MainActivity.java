01fc5c9d725afc5cb6899ce22eedd08f01f54973
wing <DongyunSky@163.com>
2018-11-21 13:47:35 +0800

    1. 测试微信分享。

--- Android/TestWeixinAPI/src/main/java/fun/chaoxi/testweixinapi/MainActivity.java
+++ Android/TestWeixinAPI/src/main/java/fun/chaoxi/testweixinapi/MainActivity.java
@@ -1,13 +1,39 @@
 package fun.chaoxi.testweixinapi;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.tencent.mm.opensdk.modelmsg.SendMessageToWX;
+import com.tencent.mm.opensdk.modelmsg.WXMediaMessage;
+import com.tencent.mm.opensdk.modelmsg.WXWebpageObject;
+import com.tencent.mm.opensdk.openapi.IWXAPI;
+import com.tencent.mm.opensdk.openapi.WXAPIFactory;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String WX_APP_ID = "wx855341fea96c1b84";
+    private IWXAPI iwxapi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        iwxapi = WXAPIFactory.createWXAPI(this, WX_APP_ID, true);
+        iwxapi.registerApp(WX_APP_ID);
+
+        WXWebpageObject webpageObject = new WXWebpageObject();
+        webpageObject.webpageUrl = "http://www.baidu.com";
+        WXMediaMessage mWxMediaMessage = new WXMediaMessage(webpageObject);
+        mWxMediaMessage.title = "title";
+        mWxMediaMessage.description = "描述";
+        SendMessageToWX.Req req = new SendMessageToWX.Req();
+        req.transaction = String.valueOf(System.currentTimeMillis());//transaction字段用于唯一标识一个请求
+        req.message = mWxMediaMessage;
+        req.scene = SendMessageToWX.Req.WXSceneSession;
+        Log.d("aaa","发送");
+        iwxapi.sendReq(req);
+
     }
 }
--- Android/TestWeixinAPI/src/main/java/fun/chaoxi/testweixinapi/WXEntryActivity.java
+++ Android/TestWeixinAPI/src/main/java/fun/chaoxi/testweixinapi/WXEntryActivity.java
@@ -1,7 +1,66 @@
 package fun.chaoxi.testweixinapi;
 
-/**
- * Create by Wing Gao on 2018/11/13 17:41
- */
-public class WXEntryActivity {
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.tencent.mm.opensdk.modelbase.BaseReq;
+import com.tencent.mm.opensdk.modelbase.BaseResp;
+import com.tencent.mm.opensdk.openapi.IWXAPI;
+import com.tencent.mm.opensdk.openapi.IWXAPIEventHandler;
+import com.tencent.mm.opensdk.openapi.WXAPIFactory;
+
+/** 微信客户端回调activity示例 */
+public class WXEntryActivity extends Activity  implements IWXAPIEventHandler {
+
+    private Context context;
+    private IWXAPI iwxapi;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        context = this;
+        iwxapi = WXAPIFactory.createWXAPI(this, "wx855341fea96c1b84", true);
+        iwxapi.handleIntent(getIntent(), this);
+    }
+
+    @Override
+    public void onReq(BaseReq req) {//发送请求时调用
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+        iwxapi.handleIntent(intent, this);//必须调用此句话
+
+    }
+
+    @Override
+    public void onResp(BaseResp resp) {
+        switch (resp.errCode) {
+            case BaseResp.ErrCode.ERR_OK:
+                //分享成功
+                Log.e("aaa", "成功");
+                Toast.makeText(context, "分享成功", Toast.LENGTH_SHORT).show();
+                this.finish();
+                break;
+            case BaseResp.ErrCode.ERR_USER_CANCEL:
+                //分享取消
+                Log.e("aaa", "错误");
+                Toast.makeText(context, "分享取消", Toast.LENGTH_SHORT).show();
+                this.finish();
+                break;
+            case BaseResp.ErrCode.ERR_AUTH_DENIED:
+                //分享拒绝
+                Log.e("aaa", "分享拒绝");
+                Toast.makeText(context, "分享拒绝", Toast.LENGTH_SHORT).show();
+                this.finish();
+                break;
+        }
+    }
+
 }
