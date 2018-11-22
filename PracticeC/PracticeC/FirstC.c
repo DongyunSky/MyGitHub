05ebae2089291f37815f166b09feb57ef6f7071c
wing <DongyunSky@163.com>
2018-11-22 21:03:43 +0800

    1. C语言第五周。

--- PracticeC/PracticeC/FirstC.c
+++ PracticeC/PracticeC/FirstC.c
@@ -2,6 +2,58 @@
 #include <math.h>
 int main()
 {
+
+    /* 计算s=1+1/2+1/3+1/4+......+1/200的值并输出（保留4位小数），正确结果为5.8780 */
+    float s = 0.0, a = 1.0;
+    for (int i = 1; i < 201; i++)
+    {
+        s += a / i;
+    }
+    printf("%.4f", s);
+
+    /* 二次三项式   ，p>0,q>0，如果常数项 q 可分解为2个因数a,b的积，并且a+b=p ，那么 就可分解为(x+a)(x+b) . */
+    int p, q, flag = 0;
+    scanf_s("%d%d", &p, &q);
+    for (int i = 1; i <= sqrt(q); i++)
+    {
+        if (q%i == 0) {
+            if (p == (i + q / i)) {
+                printf("(x+%d)(x+%d)\n", i, q / i);
+                flag = 1;
+            }
+        }
+    }
+    if (flag == 0) {
+        printf("无分解式");
+    }
+
+
+   /* 第一天训练300米，第二天训练337.5米，第三天训练379.688米，......，
+   后一天训练长度是前一天的 1+1/8倍，编程判断学生第几天训练的长度开始超过10000米？ */
+    float totalDistance = 300;
+    int day = 1;
+    while (totalDistance <= 10000)
+    {
+        totalDistance = totalDistance * (1 + 1.0 / 8);
+        day++;
+    }
+    printf("%d", day);
+
+
+    /* 输出1至1000之间所有满足用3除余2、用5除余3、用7除余2的数，要求分行输出，每行5个，并且每列左对齐。 */
+    int count = 0;
+    for (int i = 1; i < 1000; i++)
+    {
+        if (i % 3 == 2 && i % 5 == 3 && i % 7 == 2) {
+            printf("%-5d", i);
+            count++;
+        }
+        if (count == 5) {
+            printf("\n");
+            count = 0;
+        }
+    }
+
     /* 某商场清仓，商品打折销售，现需要编一个新的程序：按清仓价格计算出客户应支付的商品金额。 */
     int w, c;
     float price;
@@ -92,6 +144,9 @@ int main()
     // printf("您输入的整数是：%d；您输入的小数是：%f",a,b);
     printf("较大的数是：%f\n" , ((a + b + fabs(a - b)) / 2) );
     printf("平方根是：%f\n" , sqrt(((a + b + fabs(a - b)) / 2)));
+
+
+
 
 
 
